c575b255e823f2acbf44aca5b278daa5921016f8
felix-fy <58977279+felix-fy@users.noreply.github.com>
2021-04-07 14:48:05 +0800

    Create train_shape_model.py

---
+++ train_shape_model/train_shape_model.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import dlib
+
+#set option
+train_folder = 'photo/train'
+test_folder = 'photo/test'
+options = dlib.shape_predictor_training_options()
+options.oversampling_amount = 300
+options.nu = 0.15
+options.tree_depth = 3
+options.be_verbose = True
+
+#train model
+training_xml_path = os.path.join(train_folder, "training_with_face_landmarks.xml")
+dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)
+print("\nTraining accu\fracy: {}".format(
+    dlib.test_shape_predictor(training_xml_path, "predictor.dat")))
+
+#test model
+testing_xml_path = os.path.join(test_folder, "testing_with_face_landmarks.xml")
+print("Testing accu\fracy: {}".format(
+    dlib.test_shape_predictor(testing_xml_path, "predictor.dat")))
