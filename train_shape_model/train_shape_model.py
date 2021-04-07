619e3c43cae2bcb8453e7430bf6d623fce701cf6
felix-fy <58977279+felix-fy@users.noreply.github.com>
2021-04-07 14:59:01 +0800

    Update train_shape_model.py

--- train_shape_model/train_shape_model.py
+++ train_shape_model/train_shape_model.py
@@ -3,7 +3,7 @@ import os
 import sys
 import dlib
 
-#set option
+# set option
 train_folder = 'photo/train'
 test_folder = 'photo/test'
 options = dlib.shape_predictor_training_options()
@@ -12,13 +12,13 @@ options.nu = 0.15
 options.tree_depth = 3
 options.be_verbose = True
 
-#train model
+# train model
 training_xml_path = os.path.join(train_folder, "training_with_face_landmarks.xml")
 dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)
 print("\nTraining accu\fracy: {}".format(
     dlib.test_shape_predictor(training_xml_path, "predictor.dat")))
 
-#test model
+# test model
 testing_xml_path = os.path.join(test_folder, "testing_with_face_landmarks.xml")
 print("Testing accu\fracy: {}".format(
     dlib.test_shape_predictor(testing_xml_path, "predictor.dat")))
